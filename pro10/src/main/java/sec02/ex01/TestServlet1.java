36b91595ec1b221458b8014028102d5d415f89ec
byeongseokim <kbs970318@gmil.com>
2023-05-25 16:04:24 +0900

    여러가지 URL 패턴

---
+++ pro10/src/main/java/sec02/ex01/TestServlet1.java
@@ -0,0 +1,41 @@
+package sec02.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/first/test")
+public class TestServlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String context = request.getContextPath();
+		String url = request.getRequestURL().toString();
+		String mapping = request.getServletPath();
+		String uri = request.getRequestURI();
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<title>Test Servlet1</title>");
+		out.print("</head>");
+		out.print("<body bgcolor='green'>");
+		out.print("<b>TestServlet1입니다.</b><br>");
+		out.print("<b>컨텍스트 이름: " + context + "</b><br>");
+		out.print("<b>전체 경로: " + url + "<b><br>");
+		out.print("<b>매핑 이름: " + mapping + "<b><br>");
+		out.print("<b>URI : " + uri + "<b>");
+		out.print("</body>");
+		out.print("</html>");
+		out.close();
+
+	}
+
+}
